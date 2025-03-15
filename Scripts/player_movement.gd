25c470438f70578d1d449b1439a6ff459767fca7
Vermisso <60429652+Vermisso@users.noreply.github.com>
2025-03-15 22:53:56 +0100

    slay

--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -3,11 +3,14 @@ extends CharacterBody2D
 var speed = 600
 var rotation_speed = 3.0
 
+var current_trail: Trail
+
 @export var end_game_canvas: EndGameUI
 
 func _ready() -> void:
 	add_to_group("player")
 	Events.game_ended.connect(on_game_end)
+	make_trail()
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -37,3 +40,11 @@ func on_player_death():
 
 func on_game_end(_is_won: bool):
 	queue_free()
+	
+func make_trail() -> void:
+	if current_trail:
+		current_trail.stop()
+	current_trail = Trail.create()
+	current_trail.setColor("#ff3134")
+	add_child(current_trail)
+	
