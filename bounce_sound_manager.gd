1af57ce53418fcc9d7554139dc774cab6a3183a0
Victor Vermot <victorvermot@gmail.com>
2025-03-15 22:53:16 +0100

    test

---
+++ bounce_sound_manager.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+@onready var bouce_sound = $BounceAudio
+
+func play_bounce_sound():
+	var random_pitch = randf_range(0.8, 1.2)
+	bouce_sound.pitch_scale = random_pitch
+	bouce_sound.playing = true
