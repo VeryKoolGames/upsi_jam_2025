e034ad06d8e2861b76d131a37233c866ebbec62f
Victor Vermot <victorvermot@gmail.com>
2025-03-15 18:16:31 +0100

    swag

--- Scenes/enemy_detection.gd
+++ Scenes/enemy_detection.gd
@@ -1,7 +1,28 @@
 extends Node2D
 
-@export var warning_sprite: Sprite2D
+@export var warning_sprite: Node2D
+
+var enemy_count: int = 0
+
+func _ready() -> void:
+	warning_sprite.modulate.a = 0
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.owner.is_in_group("enemy"):
-		warning_sprite.visible = true
+		enemy_count += 1
+		_fade_warning(true)
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	if area.owner and area.owner.is_in_group("enemy"):
+		enemy_count = max(enemy_count - 1, 0)
+		if enemy_count == 0:
+			_fade_warning(false)
+
+func _fade_warning(visible: bool) -> void:
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
+	
+	if visible:
+		tween.tween_property(warning_sprite, "modulate:a", 1.0, 0.3)
+	else:
+		tween.tween_property(warning_sprite, "modulate:a", 0.0, 0.5)
--- Scenes/score_manager.gd
+++ Scenes/score_manager.gd
@@ -4,15 +4,18 @@ var score: int
 @export var score_label: Label
 @export var timer_label: Label
 @export var game_timer: Timer
+var game_length := 120
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	score_label.text = "0"
 	Events.player_scored.connect(_update_score)
 	game_timer.one_shot = true
-	game_timer.wait_time = 15
+	game_timer.wait_time = game_length
 	game_timer.start()
 	game_timer.connect("timeout", _on_timer_timeout)
+	_schedule_checkpoint(game_length * 2 / 3)
+	_schedule_checkpoint(game_length * 1 / 3)
 
 func _process(delta: float) -> void:
 	_update_timer()
@@ -27,3 +30,8 @@ func _update_score(score_to_add: int):
 
 func _on_timer_timeout():
 	Events.game_ended.emit(true)
+
+func _schedule_checkpoint(time_remaining: float) -> void:
+	var checkpoint_timer = get_tree().create_timer(game_timer.time_left - time_remaining)
+	await checkpoint_timer.timeout
+	Events.game_progressed.emit()
